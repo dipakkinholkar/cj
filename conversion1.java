0c486b449d07cbecc79a507f32fbdbbb29d12e3e
Dipak Kinholkar <39441178+dipakkinholkar@users.noreply.github.com>
2021-03-31 11:17:00 +0530

    Add files via upload

---
+++ conversion1.java
@@ -0,0 +1,14 @@
+class conversion1
+{
+	public static void main(String args[])
+	{
+	byte a=(int)254;
+	int b=(int)a;
+	System.out.print(a==b);
+		
+	byte c=(byte)b;
+		System.out.print(c==b);
+
+  	}
+
+}
